66e57564ba67872b1ef6c47d1c45ea1c60639445
Nicholas Meyers <nicholas@be-c02y81fhjg5h.lan>
2022-06-07 22:10:02 +0200

    check audience

---
+++ src/main/java/be/nicholasmeyers/backend/core/handler/AuthenticationErrorHandler.java
@@ -0,0 +1,27 @@
+package be.nicholasmeyers.backend.core.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationErrorHandler implements AuthenticationEntryPoint {
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationErrorHandler.class);
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error(authException.getMessage());
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.flushBuffer();
+    }
+}
